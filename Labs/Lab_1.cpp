63861b1e7f2e6d5504c9c07ca08a973c827979ed
RansfordGenesis <jefferykyeigenesis@gmail.com>
2023-03-15 11:04:21 +0000

    lab_1

---
+++ Labs/Lab_1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello World"<<endl;
+
+    int year = 17785;
+
+    char message[100] = "Welcome to this class!";
+    cout << message << endl;
+
+    float num = -56.1427;
+
+    string mix = "5262778y";
+
+    int a = 5, b = 5, c = 5;
+
+    bool obj = true;
+
+    
+
+    return 0;
+}
