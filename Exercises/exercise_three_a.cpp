dd3e7aebbbfb38eb1ff0f3ce6b88515a4dfe318a
RansfordGenesis <jefferykyeigenesis@gmail.com>
2023-03-18 18:41:35 +0000

    exercise_three_a

---
+++ Exercises/exercise_three_a.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+double power(double n, int p = 2);
+int main()
+{
+    double n;
+    int p;
+    cout << "Enter number\n";
+    cin >> n;
+    cout << "Enter pow\n";
+    cin >> p;
+
+    double answer = power(n, p);
+    cout << answer;
+
+    return 0;
+}
+double power(double n, int p)
+{
+    double final = 1.0;
+    for (int i=1; i<=p; i++){
+         final = final * n;
+    }
+    return final;
+}
