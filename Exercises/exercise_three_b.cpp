cefc13659dc6cbd72726c886b621fa2e3dde3d02
RansfordGenesis <jefferykyeigenesis@gmail.com>
2023-03-19 18:27:04 +0000

    exercise_three_b

--- Exercises/exercise_three_b.cpp
+++ Exercises/exercise_three_b.cpp
@@ -1,25 +1,24 @@
+//Function that takes two integers by reference and sets the smallest to zero
+
 #include <iostream>
 using namespace std;
 
-void repchar(char, int);        //function declaration
-int main()
-{
-    repchar('-', 43);           //call to function
-    cout << "Data type  Range" << endl;
-    repchar('=', 23);       //call to function
-    cout << "char   -128 to 127" << endl
-        << "short   -32,768 to 32,767" << endl
-        << "int     System dependent" << endl
-        << "double  -2,147,483,648 to 2,147,483,647" << endl;
-    repchar('-', 43);       //call to function
+void zeroSmaller(int&, int&);
+
+int main(){
+    int a, b;
+
+    cout << "Enter two integers: ";
+    cin >> a >> b;
+    
+    zeroSmaller(a, b);
+
     return 0;
 }
-//--------------------------------------------------------------
-// repchar()
-// function definition
-void repchar(char ch, int n)    //function declarator
+
+void zeroSmaller(int& a, int& b)
 {
-    for(int j=0; j<n; j++)      //function body
-        cout << ch;
-    cout << endl;
+    if (a < b) a = 0;
+    else b = 0;
+    cout << a << ", " << b << endl;
 }
