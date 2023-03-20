29aca419e8da9f4c1fe2d96fca2a35187dff7cdc
RansfordGenesis <jefferykyeigenesis@gmail.com>
2023-03-20 17:56:58 +0000

    exercise_five

---
+++ Exercises/exercise_five.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class smallobj      //define a class
+{
+private:
+int somedata;       //class data
+
+public:
+void setdata(int d)  //member function to set data
+{somedata = d;}
+void showdata()     //member function to display data
+{ cout << "Data is " << somedata << endl; }
+};
+
+int main()
+{
+    int u;
+    cin >> u;
+smallobj s1, s2, s3;            //define two objects of class smallobj
+s1.setdata(1066);
+s2.setdata(1776);//call member function to set data
+s3.setdata(u);
+
+s1.showdata();
+s2.showdata();
+s3.showdata();
+return 0;
+}
