b141d236fffbb5534c8aa4c440aaa15e9aaae01d
Ransford Genesis <83247407+RansfordGenesis@users.noreply.github.com>
2023-03-24 20:46:59 +0000

    Delete tempCodeRunnerFile.cpp

--- Exercises/tempCodeRunnerFile.cpp
+++
@@ -1,5 +0,0 @@
-cout << "Enter celsius to convert: " << endl;
-    cin >> celsius;
-
-    float fahrenheit = (celsius * val) + 32.0;
-    cout << "The equivalent in fahrenheit is: " << fahrenheit << endl;
