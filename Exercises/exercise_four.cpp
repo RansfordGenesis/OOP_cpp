f49bb011dcadc10bac717788d0471cb6c04d2b5f
RansfordGenesis <jefferykyeigenesis@gmail.com>
2023-03-25 16:12:08 +0000

    update exercise_four

--- Exercises/exercise_four.cpp
+++ Exercises/exercise_four.cpp
@@ -1,31 +1,37 @@
+//four function calculator using switch case
+
 #include <iostream>
 using namespace std;
 
 int main(){
     char ope;
-    float num1, num2, answer;
+    float num1, num2, option;
+    
+    do{
+        cout << "Enter first integer, operator and second integer\n";
+        cin >> num1 >> ope >> num2;
+    
+        switch(ope)
+        {
+            case '+':
+            cout << num1 + num2 << endl;
+            break;
 
-    cout << "Enter first integer, operator and second integer\n";
-    cin >> num1 >> ope >> num2;
+            case '-':
+            cout << num1 - num2 << endl;
+            break;
 
-    switch(ope)
-    {
-        case '+':
-        cout << num1 + num2 << endl;
-        break;
+            case '*':
+            cout << num1 * num2 << endl;
+            break;
 
-        case '-':
-        cout << num1 - num2 << endl;
-        break;
-
-        case '*':
-        cout << num1 * num2 << endl;
-        break;
-
-        case '/':
-        cout << num1 / num2 << endl;
-        break;
-    }
-
+            case '/':
+            cout << num1 / num2 << endl;
+            break;
+        }
+        cout << "Do you want to calculate again? y or n \n";
+        cin >> option;
+    }while (option == y);
+    
     return 0;
 }
