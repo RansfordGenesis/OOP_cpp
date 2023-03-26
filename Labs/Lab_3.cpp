4806dd3c47a11d818e4e32a0e41052b0f58bc8e3
RansfordGenesis <jefferykyeigenesis@gmail.com>
2023-03-26 13:04:38 +0000

    create lab_3

---
+++ Labs/Lab_3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+//function to find the sum of the first n natural numbers.
+int sum(int n){
+    int sum = 0;
+    for(int i = 1; i <= n; i++){
+        sum += i;
+    }
+    return sum;
+}
+
+int main(){
+    int n;
+    cout << "Enter the integer to sum up to: \n";
+    cin >> n;
+
+    cout << "Sum of the first " << n << "natural numbers is " << sum(n);
+    
+    return 0;
+}
