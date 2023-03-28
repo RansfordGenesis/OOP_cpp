9a86081eda5de7400c84ec2b7b77723b69c9ffdf
RansfordGenesis <jefferykyeigenesis@gmail.com>
2023-03-28 23:27:54 +0000

    update lab_3

--- Labs/Lab_3a.cpp
+++ Labs/Lab_3a.cpp
@@ -20,18 +20,5 @@ int main(){
     return 0;
 }
 
-//Function to convert height in cm to ft
-inline float cm_to_ft(float centimeter){
-    return centimeter * 0.0328084;
-}
 
-int main(){
-    float centimeter;
-    cout << "Enter height in cm: \n";
-    cin >> centimeter;
-
-    cout << "Your height in ft is: " << cm_to_ft(centimeter) << endl;
-    
-    return 0;
-}
 
