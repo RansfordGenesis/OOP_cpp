1a1ab80adb55959baef906c682c325702991a357
RansfordGenesis <jefferykyeigenesis@gmail.com>
2023-03-29 01:06:12 +0000

    update lab_3c

--- Labs/lab_3c.cpp
+++ Labs/lab_3c.cpp
@@ -1,21 +1,30 @@
+// A function to generate the Fibonacci sequence up to a given number
 #include <iostream>
 using namespace std;
 
-int fibonacci(int n){
+void fibonacci(int n){
     int first_term = 0, second_term = 1, next_term = 0;
+    
+    cout << first_term << ", " << second_term << ", ";
+    
     next_term = first_term + second_term;
+    
     while(next_term <= n){
+        cout << next_term << ", ";
+        
         first_term = second_term;
         second_term = next_term;
         next_term = first_term + second_term;
-    cout << next_term << ", " << endl;
     }
-    return next_term;
+    cout << endl;
 }
 
 int main(){
     int n;
     cin >> n;
-    cout << fibonacci(n) << ", " << endl;
+    
+    fibonacci(n);
+
+
     return 0;
 }
