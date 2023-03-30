9620962050a3238a3c0db7e6ffe680a7f14cd3ea
RansfordGenesis <jefferykyeigenesis@gmail.com>
2023-03-30 23:08:34 +0000

    update exercise_five

--- Exercises/exercise_five.cpp
+++ Exercises/exercise_five.cpp
@@ -1,3 +1,4 @@
+//Program that implements classes to do tollbooth at a bridge
 #include <iostream>
 using namespace std;
 
@@ -16,13 +17,28 @@ class tollbooth
     void nopayCar(){
         total_cars += 1;
     }
-    void display(){
+    void display() const{
         cout << "Total number of cars passes is " << total_cars << endl;
         cout << "Total money collected is " << total_money << endl;
     }
 
-
-
-
-
 };
+
+int main(){
+    tollbooth gate1;
+    char opt;
+
+    cout << "Welcome to Ayeduasi tollbooth \n ----------------------------------------------\n";
+    cout << "Select an option below: \n 1. Y - Paying car \n 2. N - Non-paying car \n 3. ESC to exit\n";
+
+    do{
+        cin >> opt;
+        if (opt == 'Y' || opt == 'y')  gate1.payingCar();
+        if (opt == 'N' || opt == 'n')  gate1.nopayCar();
+
+        }while (opt != 27);
+    
+    gate1.display();
+
+    return 0;
+}
