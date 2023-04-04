bad181d35d19e028e03e7b1a2a1a35c9621eb9fc
RansfordGenesis <jefferykyeigenesis@gmail.com>
2023-04-04 23:56:27 +0000

    Create lab_7

---
+++ Labs/lab_7.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+class CPolygon {
+protected:
+ int width, height;
+public:
+ void set_values(int a , int b){
+ width = a, height = b;
+ }
+};
+
+class CRectangle : public CPolygon{
+  public:
+      int area(){
+          return width * height;
+      }
+};
