42e48c4c84706b69c276905dbafab83e636e0e41
RansfordGenesis <jefferykyeigenesis@gmail.com>
2023-04-05 23:26:47 +0000

    Update lab_7

--- Labs/lab_7.cpp
+++ Labs/lab_7.cpp
@@ -17,3 +17,20 @@ class CRectangle : public CPolygon{
           return width * height;
       }
 };
+
+class CTriangle : public CPolygon{
+  public:
+      int area(){
+          return width * height / 2;
+      }
+};
+
+int main(){
+    CRectangle rect;
+    CTriangle tri;
+    rect.set_values(10, 5);
+    tri.set_values(10, 5);
+    cout << "Area(R) = " << rect.area() << endl;
+    cout << "Area(T) = " << tri.area() << endl;
+    return 0;
+}
