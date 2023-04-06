8160ffef75e319d3a6cb9ae03bd9f12a1d7e6cf1
RansfordGenesis <jefferykyeigenesis@gmail.com>
2023-04-06 04:39:52 +0000

    Update lab_5

--- Labs/lab_5.cpp
+++ Labs/lab_5.cpp
@@ -3,41 +3,68 @@
 
 using namespace std;
 
-class YoutubeChannel
-{
+class YoutubeChannel{
     private:
-    string Name;
-    string OwnerName;
-    int SubscriberCount;
-    list<string> PublishedVideoTitles;
+        string Name;
+        string OwnerName;
+        int SubscriberCount;
+        list<string> PublishedVideoTitles;
 
-    YoutubeChannel(): SubscriberCount(0)
-    {}
-
-    void Subscribe(){
-        SubscriberCount += 1;
-    }
-    void Unsubscribe(){
-        if (SubscriberCount > 0){
-            SubscriberCount -= 1;
-        }else{
-            SubscriberCount = 0;
-        }
-    }
-    void set_name(string name, string ownerName){
+    public: 
+        
+        YoutubeChannel(string name, string ownerName){
         Name = name;
         OwnerName = ownerName;
-    }
-    string get_name(){
-        return Name;
-        return OwnerName;
-    }
-    void PublishVideo(){
+        SubscriberCount = 0;
+        }
         
-    }
+        void Subscribe(){
+            SubscriberCount += 1;
+        }
+        
+        void Unsubscribe(){
+            if (SubscriberCount > 1)    SubscriberCount -= 1;
+        }
+        
+        void set_name(string name){
+            Name = name;
+        }
 
+        void get_name(){
+            cout << "Youtube channel name is: " << Name << endl;
+        }
 
+        void set_Ownername(string ownerName){
+            OwnerName = ownerName;
+        }
 
+        void get_Ownername(){
+            cout << "Youtuber name is: " << OwnerName << endl;
+        }
 
+        void PublishVideo(string VideoTitle){
+            PublishedVideoTitles.push_back(VideoTitle);
+        }
+
+        void GetInfo() {
+            cout << "Name: " << Name << endl;
+            cout << "Owner Name: " << OwnerName <<endl;
+            cout << "Number of Subscribers: " <<SubscriberCount <<endl;
+        }
 
 };
+
+int main()
+{
+    YoutubeChannel ytChannel("Tutorials", "Genesis");
+    ytChannel.PublishVideo("Tutorial 1");
+    ytChannel.Unsubscribe();
+
+    for (int i = 0; i < 20; i++) {
+        ytChannel.Subscribe();
+    }
+
+    ytChannel.GetInfo();
+
+    return 0;
+}
