95595b116d96203e612dfa14fbfe84e85c4e46d1
RansfordGenesis <jefferykyeigenesis@gmail.com>
2023-04-07 23:20:14 +0000

    Update lab_8

--- Labs/lab_8.cpp
+++ Labs/lab_8.cpp
@@ -26,3 +26,22 @@ class CTriangle : public CPolygon
         return width * height / 2;
     }
 };
+
+int main(){
+    CRectangle rect;
+    CTriangle trgl;
+    CPolygon poly;
+
+    CRectangle *ppoly1 = &rect;
+    CTriangle *ppoly2 = &trgl;
+    CPolygon *ppoly3 = &poly;
+
+    ppoly1-> set_values(10,5);
+    ppoly2-> set_values(10,5);
+    ppoly3-> set_values(10,5);
+
+    cout<<ppoly1-> area()<<endl;
+    cout<<ppoly2-> area()<<endl;
+    cout<<ppoly3-> area()<<endl;
+    return 0;
+}
